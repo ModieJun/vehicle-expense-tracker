2de275e5adaee59bb0e715cd6b34fea4b3754c66
Johnson <johnson.lin@ibm.com>
2025-04-12 23:13:10 +0800

    Add speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default async function Home() {
         </div>
       </header>
       <main className="container py-6">
+      <SpeedInsights/>
         <Tabs defaultValue="overview" className="space-y-6">
           <div className="flex items-center justify-between">
             <TabsList>
